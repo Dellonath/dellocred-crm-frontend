5214b3bb325aeddc6f5087cdade1719ef6943ee7
Caio Vinícius <caio1525pereira@gmail.com>
2025-07-05 04:08:56 -0300

    chore: :wrench: Configuração inicial do vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,18 @@
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
-import { defineConfig } from "vite";
+import { defineConfig, type UserConfig } from "vite";
+import type { InlineConfig } from "vitest/node";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000
+  },
+  test: {
+    globals: true,
+    setupFiles: ["./test/setup.ts"],
+    environment: "happy-dom"
   }
+} as UserConfig & {
+  test: InlineConfig;
 });
