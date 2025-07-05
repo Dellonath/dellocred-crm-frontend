48e2728f288aa6d427a30048ada65760975f1932
Caio Vinícius <caio1525pereira@gmail.com>
2025-07-05 15:08:31 -0300

    chore: :wrench: Configuração inicial do Shadcn ui

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,5 @@
+import path from "node:path";
+
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig, type UserConfig } from "vite";
@@ -7,6 +9,11 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 3000
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src")
+    }
   },
   test: {
     globals: true,
