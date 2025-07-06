1837434df2c92d5aea1abf970fcc7e7d573a027b
Caio Vinícius <caio1525pereira@gmail.com>
2025-07-06 18:59:25 -0300

    feat: :lipstick: Componente Sidebar

--- src/view/layouts/Dashboard.tsx
+++ src/view/layouts/Dashboard.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router";
 
+import { Sidebar } from "../components/Sidebar";
+
 export function DashboardLayout() {
   return (
     <div className="flex gap-1">
-      <div>Sidebar</div>
+      <Sidebar />
 
       <Outlet />
     </div>
