be0f7b25be9235ce7e7c63693b4cc7ae750f78aa
Caio Vinícius <caio1525pereira@gmail.com>
2025-07-08 20:27:07 -0300

    feat: :lipstick: Estado de erro nos inputs

--- src/view/components/Input.tsx
+++ src/view/components/Input.tsx
@@ -5,31 +5,40 @@ import { Input as ShadcnInput } from "./ui/input";
 
 interface InputProps extends ComponentProps<"input"> {
   label: string;
+  error?: string;
   labelClassName?: string;
 }
 
 export function Input({
   label,
   className,
+  error,
   labelClassName,
   ...props
 }: InputProps) {
   const id = useId();
 
   return (
-    <div className="group relative w-full">
-      <label
-        htmlFor={id}
-        className="origin-start text-muted-foreground/70 group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:text-foreground absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium"
-      >
-        <span
-          className={twMerge("bg-background inline-flex px-2", labelClassName)}
+    <div className="w-full space-y-2">
+      <div className="group relative w-full">
+        <label
+          htmlFor={id}
+          className="origin-start text-muted-foreground/70 group-focus-within:text-foreground has-[+input:not(:placeholder-shown)]:text-foreground absolute top-1/2 block -translate-y-1/2 cursor-text px-1 text-sm transition-all group-focus-within:pointer-events-none group-focus-within:top-0 group-focus-within:cursor-default group-focus-within:text-xs group-focus-within:font-medium has-[+input:not(:placeholder-shown)]:pointer-events-none has-[+input:not(:placeholder-shown)]:top-0 has-[+input:not(:placeholder-shown)]:cursor-default has-[+input:not(:placeholder-shown)]:text-xs has-[+input:not(:placeholder-shown)]:font-medium"
         >
-          {label}
-        </span>
-      </label>
+          <span
+            className={twMerge(
+              "bg-background inline-flex px-2",
+              labelClassName
+            )}
+          >
+            {label}
+          </span>
+        </label>
 
-      <ShadcnInput id={id} placeholder=" " className={className} {...props} />
+        <ShadcnInput id={id} placeholder=" " className={className} {...props} />
+      </div>
+
+      {!!error && <span className="text-xs text-red-400">{error}</span>}
     </div>
   );
 }
