69825193d7d73ad9e3e4b8c8880e6765a2d0e57f
Caio Vinícius <caio1525pereira@gmail.com>
2025-07-08 19:47:30 -0300

    refactor: :label: Alteração na tipagem da entidade Client

--- src/app/entities/Client.ts
+++ src/app/entities/Client.ts
@@ -1,10 +1,78 @@
+type ChannelType = "online" | "offline";
+
+type Gender = "m" | "f";
+
+type MaritialStatus = "single" | "married" | "divorced" | "windowed";
+
+type EducationLevel =
+  | "primary"
+  | "secondary"
+  | "high_school"
+  | "bachelor"
+  | "master"
+  | "doctorate";
+
+type ClientSector = "private" | "public";
+
+type UtmSource = "google" | "facebook" | "instagram" | "email" | "direct";
+
+type UtmMedium = "cpc" | "organic" | "social" | "email" | "referral";
+
+type State =
+  | "ac"
+  | "ap"
+  | "am"
+  | "pa"
+  | "ro"
+  | "rr"
+  | "to"
+  | "al"
+  | "ba"
+  | "ce"
+  | "ma"
+  | "pb"
+  | "pe"
+  | "pi"
+  | "rn"
+  | "se"
+  | "df"
+  | "go"
+  | "mt"
+  | "ms"
+  | "es"
+  | "mg"
+  | "rj"
+  | "sp"
+  | "pr"
+  | "rs"
+  | "sc";
+
 export interface Client {
-  id: string;
-  cpf: string;
-  name: string;
+  uuid: string;
+  govId: string;
+  firstName: string;
+  lastName: string;
   email: string;
-  phone: string;
-  gender: "F" | "M";
-  address: string;
-  status: "Active" | "Inactive";
+  phoneNumber: string;
+  channelType?: ChannelType;
+  birthDate?: string;
+  gender?: Gender;
+  occupation?: string;
+  maritialStatus?: MaritialStatus;
+  educationLevel?: EducationLevel;
+  wage?: number;
+  clientSector?: ClientSector;
+  country?: string;
+  state?: State;
+  city?: string;
+  addressNeighborhood?: string;
+  addressStreet?: string;
+  addressNumber?: number;
+  addressComplement?: string;
+  postalCode?: string;
+  utmSource?: UtmSource;
+  utmMedium?: UtmMedium;
+  utmCampaign?: string;
+  createdByUserUuid?: string;
+  isActive?: boolean;
 }
