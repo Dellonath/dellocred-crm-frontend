e2cef1af230f0b253f0e52eb196cc76dba32fcfa
Caio Vinícius <caio1525pereira@gmail.com>
2025-07-08 20:25:50 -0300

    chore: :wrench: Adição do provider do tanstack query

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
+import { QueryClientProvider } from "@tanstack/react-query";
 import { RouterProvider } from "react-router";
 
+import { queryClient } from "./app/lib/tanstackQuery";
 import { router } from "./router";
 
 export function App() {
-  return <RouterProvider router={router} />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />;
+    </QueryClientProvider>
+  );
 }
